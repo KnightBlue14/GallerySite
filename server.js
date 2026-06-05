a3b47e2ad89db3c4ecc689b7d599d430896e94c9
KnightBlue <domburton@hotmail.co.uk>
2026-06-05 14:31:45 +0100

    updated

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5173;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Container!' });
+  res.json({ message: 'Hello from the Gallery Container!' });
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
